1fb897520e986d5cee9c87586462a8ec710ac8df
Takoh Israel <iminuifuong@gmail.com>
2025-07-17 09:32:36 -0700

    updated back

--- server.js
+++ server.js
@@ -103,12 +103,13 @@ const findAvailablePort = (startPort) => {
 // CORS configuration with dynamic origin
 const corsOptions = {
   origin: [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:3003',
-    'http://localhost:3004',
-    'http://localhost:3005'
+    'https://voltech.onrender.com'
+    // 'http://localhost:3000',
+    // 'http://localhost:3001',
+    // 'http://localhost:3002',
+    // 'http://localhost:3003',
+    // 'http://localhost:3004',
+    // 'http://localhost:3005'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
